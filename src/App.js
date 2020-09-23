67b1f9c6b13fa5d150ad49c0a2027c882bb48be9
Omar Ahmed <omar.ahmed.oaaem@gmail.com>
2020-09-23 21:19:48 +0200

    feat(add-invoice): load html file and download it as pdf

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ const useStyles = makeStyles((theme) => ({
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
   },
 }));
 
--- src/pages/AddInvoice.js
+++ src/pages/AddInvoice.js
@@ -1,30 +1,43 @@
-import { Grid, makeStyles, Paper } from '@material-ui/core';
-import React from 'reactn';
+import { Button, Grid, makeStyles, Paper } from '@material-ui/core';
+import React, { useState } from 'reactn';
+import renderHTML from 'react-render-html';
+import InvoiceTemplate from '../templates/invoice-sparksuite.html';
+
+const pdf = window.require('html-pdf')
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+    height: '100vh'
+  }
 }));
 
 function AddInvoice() {
   const classes = useStyles();
+  const prepareHtml = () => {
+    try {
+      return renderHTML(InvoiceTemplate)
+    } catch(e) {
+      console.log(e);
+    }
+  }
+  const downloadPdf = () => {
+    pdf.create(InvoiceTemplate).toFile('/home/mrgeek/Desktop/invoic.pdf', (err, res) => {
+      console.log(res.filename);
+    });
+  }
   
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            Add Invoice
-          </Paper>
-        </Grid>
+    <Grid container className={classes.root}>
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Button onClick={downloadPdf}>
+          Download PDF
+        </Button>
       </Grid>
-    </div>
+      <Grid item xs={false} sm={4} md={7}>
+        {prepareHtml()}
+      </Grid>
+    </Grid>
   )
 }
 
